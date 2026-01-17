18280b91b1a098bfee53b87107df22113ec5ca04
Aditya <kotteaditya919@gmail.com>
2026-01-17 12:25:32 +0100

    Re-organisation of the repo  (#1)
    * removed object detection folder
    
    * added init and test 2 to original object detection folder
    
    * added test2 flie to Location tracking folder
    
    * moved npy flies to new folder
    
    * Removed the npy files
    
    * comitted tracking_code with video
    
    ---------
    
    Co-authored-by: horvathbala <balazs.horvath@tum.de>
    Co-authored-by: Youssef Sabry <y_sabry@outlook.com>
    Co-authored-by: Lina Ghonim <linaaghonim@gmail.com>

--- Location Tracking/tracking_code_trial2.py
+++ Location Tracking/tracking_code_trial2.py
@@ -5,7 +5,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-from object_detection.test2 import detect_two_images
+from test2 import detect_two_images
 import glob
 import numpy as np
 import cv2
--- object_detection/analysis.py
+++
@@ -1,9 +0,0 @@
-# pyright: reportPrivateImportUsage=false
-from ultralytics import YOLO
-
-# Load the trained models
-model_n = YOLO("runs/detect/train/weights/best.pt")
-model_s = YOLO("runs/detect/train2/weights/best.pt")
-
-model_n.val()
-model_s.val()
